14ef49d4965511b5c40dda2b37b4380215988c4c
Rasa Muktupavela <31621503+RasaMukti@users.noreply.github.com>
2021-07-21 17:22:09 +0200

    Add files via upload

--- stepadna.R
+++ stepadna.R
@@ -9,12 +9,19 @@ option_list = list(
   make_option(c("-c", "--cores"), type="integer", default=1,
               help="number of cores to us in parallel"),
   make_option(c("-i", "--init"), type="integer", default=2,
-              help="number of initial points for simulated annealing")
+              help="number of initial points for simulated annealing"),
+  make_option(c("-l", "--loglik"), type="character", default="ph",
+              help="whether to use pseudohaploid genotypes (ph) or genotype likelihoods (gl)"),
+  make_option(c("--lon"), type="numeric", default=NULL,
+              help="longitude of the origin of the allele. Has to be in the range from -10 to 80"),
+  make_option(c("--lat"), type="numeric", default=NULL,
+              help="latitude of the origin of the allele. Has to be in the range from 30 to 75"),
+  make_option(c("--seed"), type="numeric", default=5,
+              help="Seed for optimization")
 );
 
 opt_parser = OptionParser(option_list=option_list);
 opt = parse_args(opt_parser);
-
 
 library(deSolve)
 library(ReacTran)
@@ -43,7 +50,7 @@ options(scipen = 999)
 la1 <- 30
 la2 <- 75
 lo1 <- -10
-lo2 <- 100
+lo2 <- 80
 
 # Create topographical data grid
 topo  <- createTopo(la1, la2, lo1, lo2)
@@ -68,19 +75,23 @@ Latpoints <- rep(latseq, each=length(lonseq))
 ptsremoved <- c(2,3,4,6,8,10,11,13,14,16,18,20,21,22,23,26,28,30,32,33,34,36,38,40,42,43,44,45,46,48,50,52,54,55,56,57,58,60,62,64,65,66,67,68,70,72,74,76,77)
 Lonpoints <- Lonpoints[-ptsremoved]
 Latpoints <- Latpoints[-ptsremoved]
-
+Lonpoints[25] <- Lonpoints[25]+1
+Lonpoints[14] <- Lonpoints[14]+1
 
 # Initialise parameters
 Nx <- nrow(topo)
 Ny <- ncol(topo)
 D = 2.5 # Initial population density
-timeP = round(opt$age/10) 
+timeP = round(opt$age/290) 
 timeSplit = round(5000/290)
 
-######################################## MLE ######################################## 
+x0_fixed <- Nx+1-which(topoLat == closest(topoLat, opt$lat))
+y0_fixed <- which(topoLon == closest(topoLon, opt$lon))
+
+######################################## MLE PH ######################################## 
 
 # Function for calculating likelihoods
-LL_2step <- function(iter, Nx=Nx, Ny=Ny, s=s, sigmax=sigmax, sigmay=sigmay, D=D, x0=x0, y0=y0, xtran=xtran, ytran=ytran, timeP=timeP, timeSplit=timeSplit, oldnew=NULL){
+LL_PH <- function(iter, Nx=Nx, Ny=Ny, s=s, sigmax=sigmax, sigmay=sigmay, D=D, x0=x0, y0=y0, xtran=xtran, ytran=ytran, timeP=timeP, timeSplit=timeSplit, oldnew=NULL){
   
   # Set boundaries for SANN algorithm
   s.low <- 0
@@ -92,7 +103,7 @@ LL_2step <- function(iter, Nx=Nx, Ny=Ny, s=s, sigmax=sigmax, sigmay=sigmay, D=D,
   if (s < s.low | s > s.high | sigmax < sigma.low | sigmay < sigma.low |
       sigmax > sigma.high | sigmay > sigma.high |
       xtran < tran.low | ytran < tran.low | xtran > tran.high | ytran > tran.high
-      | x0 < 1 | y0 < 1 | x0 > Nx | y0 > Ny){
+      | x0 < 1 | y0 < 1 | x0 > Nx | y0 > Ny | (topo[x0,y0] < 0) == TRUE){
     return(c(99999))
   } else{
 
@@ -178,11 +189,104 @@ LL_2step <- function(iter, Nx=Nx, Ny=Ny, s=s, sigmax=sigmax, sigmay=sigmay, D=D,
   }
 }
 
+######################################## MLE GL######################################## 
+
+# Function for calculating likelihoods
+LL_GL <- function(iter, Nx=Nx, Ny=Ny, s=s, sigmax=sigmax, sigmay=sigmay, D=D, x0=x0, y0=y0, xtran=xtran, ytran=ytran, timeP=timeP, timeSplit=timeSplit, oldnew=NULL){
+  
+  # Set boundaries for SANN algorithm
+  s.low <- 0
+  s.high <- 0.1
+  sigma.low <- 1
+  sigma.high <- 100
+  tran.low <- -2.5
+  tran.high <- 2.5
+  if (s < s.low | s > s.high | sigmax < sigma.low | sigmay < sigma.low |
+      sigmax > sigma.high | sigmay > sigma.high |
+      xtran < tran.low | ytran < tran.low | xtran > tran.high | ytran > tran.high
+      | x0 < 1 | y0 < 1 | x0 > Nx | y0 > Ny | (topo[x0,y0] < 0) == TRUE){
+    return(c(99999))
+  } else{
+    
+    x0 <- round(x0)
+    y0 <- round(y0)
+    
+    if (oldnew == 0){
+      out <- FischerSolver(Nx, Ny, s, sigmax, sigmay, D, x0, y0, xtran, ytran, (timeP-timeSplit), oldnew = "old")
+      out <- out[,2:ncol(out)]
+      
+      time_periods <- seq(timeP,timeSplit,-1)
+      timevect <- timeP+1 - time_periods
+    }
+    
+    if (oldnew == 1){
+      out <- FischerSolver(Nx, Ny, s, sigmax, sigmay, D, x0, y0, xtran, ytran, timeSplit, oldnew="new", initMat=initMat)
+      out <- out[,2:ncol(out)]
+      
+      time_periods <- seq(timeSplit,0,-1)
+      timevect <- timeSplit+1 - time_periods
+    }
+    
+    l <- 0
+    # Optimizer crashes with high s values
+    if (nrow(out) != length(time_periods)){
+      print("ERRORRRRR")
+      return(c(100000))
+    }
+    else{
+      
+      for (tp in 1:length(timevect)){
+        
+        mat_out <- matrix(unlist(out[timevect[tp],]),nrow=Nx,ncol=Ny)
+        p <- t(mat_out[nrow(mat_out):1,])
+        current_time <- ts_data[which(ts_data$age == time_periods[tp]),]
+          
+        # Genotype likelihood calculations
+        if (nrow(current_time) != 0){
+          for (i in 1:nrow(current_time)){
+              
+            # Extract spatial points matching observed point
+            y_topo <- which(topoLon == closest(topoLon, current_time$longitude[i]))[1]
+            x_topo <- which(topoLat == closest(topoLat, current_time$latitude[i]))[1]
+            
+            tl <- 0
+            n <- 2
+            for (k in c(0,1,2)){  # k represetns number of derived alleles
+              
+              if (k == 0){ ll <- current_time$AA[i]}
+              if (k == 1){ ll <- current_time$AD[i]}
+              if (k == 2){ ll <- current_time$DD[i]}
+              
+              tl <- tl + ll*choose(n, k)*p[y_topo,x_topo]^k*(1-p[y_topo,x_topo])^(n-k)
+            }
+            l <- l+log(tl)
+            }
+          }
+      }
+      
+      print(round(c(s, sigmax, sigmay, xtran, ytran, -l), 3))
+      
+      if (is.nan(-l)){
+        return(c(99999))
+      } else{
+        return(c(-l))}
+    }
+  }
+}
 
 #########################################################################################
+if(opt$loglik == "ph"){
+  func <- LL_PH
+} else if (opt$loglik == "gl"){
+  func <- LL_GL
+}
+
 start_time <- Sys.time()
-ResOld <- param_optim_old(init_points = opt$init, seed = 5, num_cores = opt$cores)
-CIres <- getCI(ResOld, "old")
+if (is.null(opt$lat) == FALSE & is.null(opt$lon) == FALSE){
+  ResOld <- param_optim_fixedInit(init_points = opt$init, seed = opt$seed, num_cores = opt$cores, LL=func, oldnew="old")
+} else{ResOld <- param_optim_old(init_points = opt$init, seed = opt$seed, num_cores = opt$cores, LL=func)}
+
+CIres <- getCI(ResOld, "old", LL=func)
 bestResOld <- CIres[1][[1]]
 oldCIvect <- CIres[2:length(CIres)]
 
@@ -191,8 +295,11 @@ outOld <- outOld[,2:ncol(outOld)]
 initMat <- matrix(unlist(outOld[nrow(outOld),]),nrow=Nx,ncol=Ny)
 initMat[which(topo < 0, arr.ind = TRUE)] <- 0
 
-ResNew <- param_optim_new(init_points = opt$init, seed = 5, num_cores = opt$cores)
-CIresNew <- getCI(ResNew, "new")
+if (is.null(opt$lat) == FALSE & is.null(opt$lon) == FALSE){
+  ResNew <- param_optim_fixedInit(init_points = opt$init, seed = opt$seed, num_cores = opt$cores, LL=func, oldnew="new")
+} else{ResNew <- param_optim_new(init_points = opt$init, seed = opt$seed, num_cores = opt$cores, LL=func)}
+
+CIresNew <- getCI(ResNew, "new", LL=func)
 bestResNEW <- CIresNew[1][[1]]
 newCIvect <- CIresNew[2:length(CIresNew)]
 end_time <- Sys.time()
@@ -216,6 +323,7 @@ outDF <- rbind(outDF, data.frame(. = c(paste("after 5000"), "CI low", "CI high")
                     ytran=c(round(coef(bestResNEW)[["ytran"]], 3), newCIvect[[9]], newCIvect[[10]]),
                     longitude=round(topoLon[coef(bestResNEW)[["y0"]]]), latitude=round(topoLat[Nx+1-coef(bestResNEW)[["x0"]]]), alleleAge.years=timeP*290))
 
-write.csv(outDF, opt$out, quote=FALSE)
 
+write.csv(outDF, opt$out, quote=FALSE)
+save.image(paste0(opt$out,".RData"))
 
